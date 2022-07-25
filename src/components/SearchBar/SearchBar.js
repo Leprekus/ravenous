c9fa6b82c76868655b5226f66966f87fcc9a0038
Leprekus <leprekus@yahoo.com>
2022-07-24 21:12:01 -0700

    add object to make requests to yelp api

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBar.css';
+
+//should follow:
+//formatting conventions
+//naming conventions
+
+const sortByOptions = {
+    'bestMatch': 'best_match',
+    'highestRated': 'rating',
+    'mostReviewed': 'review_count',
+}
